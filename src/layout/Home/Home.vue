74838681332c352222dc740d7e4c0ff0e8e1d16f
yulc <350009335@qq.com>
2019-11-15 23:31:42 +0800

    element-ui

--- src/layout/Home/Home.vue
+++ src/layout/Home/Home.vue
@@ -1,13 +1,24 @@
 <template>
-
+    <el-container>
+        <el-aside width="200px">
+            <SideBar/>
+        </el-aside>
+        <el-container>
+            <el-header>
+                <HeadBar/>
+            </el-header>
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
+    import SideBar from "../../layout/SideBar/SideBar";
+
     export default {
-        name: 'Home'
+        name: "Home",
+        components: {SideBar}
     }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
