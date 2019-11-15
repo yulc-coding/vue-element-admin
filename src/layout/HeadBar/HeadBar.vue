26487324439aced363d0622b0d678052b4ffb912
yulc <350009335@qq.com>
2019-11-15 23:21:43 +0800

    init

---
+++ src/layout/HeadBar/HeadBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="head-bar">
+        <div class="head-right">
+            Header
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HeadBar'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layout/SideBar/SideBar.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <!--logo-->
+        <div class="logo">
+            <router-link to="/home">
+                <img :src="require('@/assets/logo.png')" alt="后台管理">
+            </router-link>
+        </div>
+        <!--导航菜单 -->
+        <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
+                 :collapse="isCollapse">
+            <el-submenu index="1">
+                <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span slot="title">导航一</span>
+                </template>
+                <el-menu-item-group>
+                    <span slot="title">分组一</span>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                    <span slot="title">选项4</span>
+                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+            </el-menu-item>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SideBar'
+    }
+</script>
+
+<style scoped>
+
+</style>
