266d32994c0b2ed2bf7e59f54c4eebbeaf6ded81
yulc <350009335@qq.com>
2020-03-21 22:45:48 +0800

    高度调整

--- src/layout/Index/Index.vue
+++ src/layout/Index/Index.vue
@@ -49,7 +49,7 @@
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    height: 100%;
+    height: 100vh;
   }
 
   .index-box .el-container {
